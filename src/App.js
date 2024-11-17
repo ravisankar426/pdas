862388ed57f5b01d0bb2c11acd5773e4bb2859f1
ravisankar-uppu <ravisankar_uppu@yahoo.co.in>
2024-11-17 02:05:54 -0600

    commit for the pdas app.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+import "bulma/css/bulma.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard title="Siri" handle="@siri44" image={SiriImage} />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
